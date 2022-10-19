11b572be0d5fec8653b40190e7d09cb4121eddbd
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2022-10-19 19:34:25 +0530

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const connectToMongo = require('./database');
+const express = require('express');
+
+connectToMongo();
+
+
+// setting up express server
+
+const app = express();
+const port = 9000;
+
+// We use this middle ware to read request body params
+app.use(express.json());
+
+// Avialable Routes
+//app.use('/api/auth', require('./routes/auth'));
+//app.use('/api/notes', require('./routes/notes'));
+
+app.listen(port, ()=>{
+    console.log("Server started at port: "+port);
+})

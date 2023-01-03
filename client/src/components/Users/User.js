841cb29440d0bea09f0bdf3aac931768ad90d93f
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-01-03 15:27:05 +0530

    added modal before deleting

--- client/src/components/Users/User.js
+++ client/src/components/Users/User.js
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { useNavigate } from 'react-router-dom';
 import SpecificuserContext from '../../context/specificuser/specificuserContext';
+import AlertContext from '../../context/alerts/alertContext';
 
 const User = (props) => {
     const {_id, name, employee_id, admin, date} = props.user;
@@ -8,8 +9,13 @@ const User = (props) => {
     let navigate = useNavigate();
 
     // This is for Alert Context
+    const alertContext = useContext(AlertContext);
+    const { updateAlert } = alertContext;
+
     const context= useContext(SpecificuserContext);
     const { updateUser, deleteUser } = context;
+
+    const [state, setState] = useState({showModal: false});
 
     function handleClick(event){
       event.preventDefault();
@@ -20,9 +26,20 @@ const User = (props) => {
     const handleDelete = async ()=>{
       await deleteUser(_id);
       props.fetchUsers();
+      setState({showModal: false});
+      updateAlert("Employee \""+ name +" \"deleted successfully", "warning");
+    }
+
+    const handleCloseModal = ()=>{
+      setState({showModal: false});
+    }
+
+    const toggleModal = ()=>{
+      setState({showModal: !state.showModal});
     }
 
   return (
+    <>
         <tr className='user-row'>
             <td onClick={handleClick} className="name">
               <h6>
@@ -45,12 +62,23 @@ const User = (props) => {
               </h6>
             </td>
             <td>
-              <span onClick={handleDelete}>
+              <span onClick={toggleModal}>
             <i className="fa-solid fa-trash fa-lg"></i>
             </span>
             </td>
           </tr>
-        
+          <div>
+        {state.showModal && (
+          <div className="">
+          <div className="modal-body">
+            <div>Are you sure you want to delete the employee "{name}"?</div>
+            <button className="btn btn-primary" onClick={handleDelete}>Yes</button>
+            <button className="btn btn-secondary mx-2" onClick={handleCloseModal}>No</button>
+          </div>
+        </div>
+        )}
+      </div>
+        </>
   )
 }
 

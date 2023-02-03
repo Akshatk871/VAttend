7577f35096b0541c1e2cd4900ae98704d998eddb
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-02-04 00:58:22 +0530

    Added Search Feature in Dashboard

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -25,6 +25,7 @@ const Users = () => {
     const json = await response.json();
     if (json.success) {
       updateUsers(json.users);
+      setUsersFiltered(json.users);
     } else {
       updateAlert(json.error, "danger");
     }
@@ -35,10 +36,33 @@ const Users = () => {
     // eslint-disable-next-line
   }, []);
 
+  const [Search, setSearch] = useState("");
+  const [usersFiltered, setUsersFiltered] = useState([]);
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const searchUser = async () => {
+    // search for user based on name or employee id
+    setUsersFiltered(users.filter((user) => user.name.toLowerCase().includes(Search.toLowerCase()) || user.employee_id.toLowerCase().includes(Search.toLowerCase())));
+  };
+
+  const handleSearch = () => {
+    searchUser(Search);
+  };
+
   return (
     <>
       <div className="user">
         <h1>All Users</h1>
+
+        <div className="filter">
+          <input type="text" placeholder="Search" onChange={handleSearchChange}/>
+          <button className="btn btn-dark btn-sm mx-2 my-2" onClick={handleSearch}>Search</button>
+          <button className="btn btn-dark btn-sm" onClick={fetchUsers}>Reset</button>
+        </div>
+
         <div className="user-section table-responsive-sm">
           <table className="table table-bordered">
             <thead class="thead-dark">
@@ -53,7 +77,7 @@ const Users = () => {
 
             {/* This is the individual users */}
 
-            {users.map((user, index) => {
+            {usersFiltered.map((user, index) => {
               return <User user={user} key={index} fetchUsers={fetchUsers} />;
             })}
 
@@ -62,6 +86,8 @@ const Users = () => {
 
           <br />
         </div>
+
+        <p>**Showing <strong>{usersFiltered.length}</strong> Results**</p>
       </div>
     </>
   );
